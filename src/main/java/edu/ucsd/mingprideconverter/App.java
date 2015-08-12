92d753c33bea6bdc9b9b0707737396ef4d2852d0
mingwang <miw023@cs.ucsd.edu>
2015-08-12 16:23:33 -0700

    adding data and code to do conversion

--- src/main/java/edu/ucsd/mingprideconverter/App.java
+++ src/main/java/edu/ucsd/mingprideconverter/App.java
@@ -1,5 +1,14 @@
 package edu.ucsd.mingprideconverter;
 
+import java.io.File;
+
+import uk.ac.ebi.pride.jmztab.model.MZTabFile;
+import uk.ac.ebi.pride.jmztab.utils.MZTabFileConverter;
+import uk.ac.ebi.pride.utilities.data.controller.impl.ControllerImpl.PrideXmlControllerImpl;
+import uk.ac.ebi.pride.utilities.data.exporters.AbstractMzTabConverter;
+import uk.ac.ebi.pride.utilities.data.exporters.PRIDEMzTabConverter;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +17,27 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println(args[1]);
+        String prideXMLFilename = args[1];
+        //writeMGF(prideXMLFilename, "output.mgf");
+        writeMzTab(prideXMLFilename, "output.mzTab");
+    }
+    
+    public static int writeMzTab(String prideXMLFilename, String output_mzTab){
+    	File inputFile = new File(prideXMLFilename);
+    	PrideXmlControllerImpl prideController = new PrideXmlControllerImpl(inputFile);
+    	AbstractMzTabConverter mzTabconverter = new PRIDEMzTabConverter(prideController);
+    	MZTabFile mzTabFile = mzTabconverter.getMZTabFile();
+        MZTabFileConverter checker = new MZTabFileConverter();
+        checker.check(mzTabFile);
+        System.out.println(mzTabFile.toString());
+    	return 0;
+    }
+    
+    public static int writeMGF(String prideXMLFilename, String output_MGF){
+    	File inputFile = new File(prideXMLFilename);
+    	PrideXmlControllerImpl prideController = new PrideXmlControllerImpl(inputFile);
+    	//MGFConverter converter = new MGFConverter(prideController, prideController);
+    	return 0;
     }
 }
